41ef6188bab14e5d4903682e4e8fec08a5f23f36
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-10-15 16:59:19 -0700

    created sql databases

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Burger");
+INSERT INTO burgers (burger_name) VALUES ("Chicken Burger");
+INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
