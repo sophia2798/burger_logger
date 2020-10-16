48b0d5650764d346b2472b16d045d1c06e0f5a8a
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-10-15 20:26:27 -0700

    completed server.js file

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+// DEPENDENCIES & GLOBAL VARIABLES
 const express = require("express");
+const exphbs = require("express-handlebars");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// EXPRESS SETTINGS
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.engine("handlebars", exhbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// IMPORT ROUTES
+const routes = require("./controllers/burgers_controllers");
+app.use(routes);
+
+// LISTEN
+app.listen(PORT, () => {
+    console.log(`Server listening on: http://localhost:${PORT}`);
+})
